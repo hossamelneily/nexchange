5baba015789e97ba51b58b4cbf91ddbbac7c689e
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-05-25 02:59:10 -0300

    withdraw_address: Adds tests for AJAX requests on. #TIME 1H

--- core/tests.py
+++ core/tests.py
@@ -3,6 +3,9 @@ from django.core.exceptions import ValidationError
 from core.validators import validate_bc
 from .models import Currency, Order
 from django.contrib.auth.models import User
+from django.test import Client
+from django.core.urlresolvers import reverse
+from django.utils import translation
 
 
 class ValidateBCTestCase(TestCase):
@@ -69,3 +72,71 @@ class PlaceOrderTestCase(TestCase):
 
         fetch_order = Order.objects.filter(withdraw_address=None).count()
         self.assertEqual(fetch_order, 1)
+
+
+class UpdateWithdrawAddressTestCase(TestCase):
+
+    def setUp(self):
+        """Create user and authenticate request"""
+        username = '+555190909898'
+        password = '123Mudar'
+        user = User.objects.create_user(username=username, password=password)
+        self.client = Client()
+        self.client.login(username=username, password=password)
+
+        translation.deactivate_all()
+        translation.activate('en')
+
+        """Creates an order"""
+        currency = Currency(code='RUB', name='Russian Rubles')
+        currency.save()
+
+        data = {
+            'amount_cash': 30674.85,
+            'amount_btc': 1,
+            'currency': currency,
+            'user': user,
+            'admin_comment': 'test Order',
+            'wallet': 'what goes here?',
+            'unique_reference': '12345',
+            'withdraw_address': '17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j'
+        }
+
+        order = Order(**data)
+        order.full_clean()  # ensure is initially correct
+        order.save()
+        self.order = order
+
+        # URL where to POST
+        pk = self.order.pk
+        self.url = reverse('core.update_withdraw_address', kwargs={'pk': pk})
+
+    def test_forbiden_to_update_other_users_orders(self):
+        username = '+555190909100'
+        password = '321Changed'
+        user = User.objects.create_user(username=username, password=password)
+        client = Client()
+        client.login(username=username, password=password)
+
+        response = client.post(self.url, {
+            'pk': self.order.pk,
+            'value': '17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j', })
+
+        self.assertEqual(403, response.status_code)
+
+    def test_sucess_to_update_withdraw_adrress(self):
+        response = self.client.post(self.url, {
+            'pk': self.order.pk,
+            'value': '17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j', })
+
+        self.assertJSONEqual('{"status": "OK"}', str(
+            response.content, encoding='utf8'),)
+
+    def test_error_to_update_withdraw_adrress_with_invalida_data(self):
+        response = self.client.post(self.url, {
+            'pk': self.order.pk,
+            'value': 'invalid data', })
+
+        expected = '{"status": "ERR", "msg": "invalid data ' + \
+            'has invalid characters for a valid bit coin address"}'
+        self.assertJSONEqual(expected, str(response.content, encoding='utf8'),)
