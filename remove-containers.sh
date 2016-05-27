d9d2f6cc12ce81962d8e3e92b613569d6c9b486a
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-05-27 06:54:04 -0300

    wercker:  explicitly add 'sudo' on ever docker command executed during deploy.

--- remove-containers.sh
+++ remove-containers.sh
@@ -1,3 +1,3 @@
 # stop and remove existent containers          
-docker stop $(docker ps -q --filter "name=$1") 2>/dev/null
-docker rm $(docker ps -q -a --filter "name=$1") 2>/dev/null
+sudo docker stop $(docker ps -q --filter "name=$1") 2>/dev/null
+sudo docker rm $(docker ps -q -a --filter "name=$1") 2>/dev/null
