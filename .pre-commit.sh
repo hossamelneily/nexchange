81d5f13494c125287c079395a56bd0bb8ec10845
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-06-12 20:54:58 -0300

    Only call static validation if thereis .py in commit.

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-wercker build --direct-mount --pipeline static-validation &&
+FILES_CACHED=$(git diff --cached --name-only --diff-filter=ACM | grep -e '\.py$')
+FILES=$(git diff --name-only --diff-filter=ACM | grep -e '\.py$')
+
+
+if [ -n "$FILES" ] || [ -n "$FILES_CACHED" ] ; then
+    wercker build --direct-mount --pipeline static-validation &&
+        wercker build --direct-mount --pipeline tests
+else
+    echo "======================="
+    echo "======= No .py FILES TO COMMIT. SKIPING STATIC VALIDATION. "
+    echo "======================="
     wercker build --direct-mount --pipeline tests
+fi
--- static-validation.sh
+++ static-validation.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 # Auto-check for pep8 so I don't check in bad code
 
+FILES_CACHED=$(git diff --cached --name-only --diff-filter=ACM | grep -e '\.py$')
 FILES=$(git diff --name-only --diff-filter=ACM | grep -e '\.py$')
+
+
 OPTIONS="--ignore=F403"
 
 
-if [ -n "$FILES" ]; then
+if [ -n "$FILES" ] || [ -n "$FILES_CACHED" ] ; then
     echo "======================="
-    echo "======= CHECKING FILES: $FILES"
+    echo "======= CHECKING FILES: $FILES $FILES_CACHED"
     echo "======================="
-    flake8 $OPTIONS $FILES
-else
-    echo "======================="
-    echo "======= No .py FILES TO COMMIT. SKIPING STATIC VALIDATION. "
-    echo "======================="
+    flake8 $OPTIONS $FILES $FILES_CACHED
 fi
