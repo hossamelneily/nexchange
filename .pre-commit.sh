8e650f61a62ff30c23f830c26fa8b2d63f6cfa71
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-06-12 19:00:55 -0300

    Check if static validation is really necessary.

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-wercker build --direct-mount --docker-local --pipeline static-validation &&
-wercker build --direct-mount --docker-local --pipeline tests
+FILES=$(git diff --name-only --diff-filter=ACM | grep -e '\.py$')
+if [ -n "$FILES" ]; then
+    echo "======= RUNNING STATIC FILES VALIDATION"
+    wercker build --direct-mount --pipeline static-validation
+    status=$?
+else 
+    echo "======= NO .py FILES ON THIS COMMIT"
+fi
+
+if [ "$status" == "0" ]; then
+    echo "======= RUNNING APP TESTS"
+    wercker build --direct-mount --pipeline tests
+    status=$?
+fi
+
+exit $status
 
--- static-validation.sh
+++ static-validation.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # Auto-check for pep8 so I don't check in bad code
-FILES=$(git diff --cached --name-only --diff-filter=ACM | grep -e '\.py$')
 
-echo "FILES: $FILES"
+FILES=$(git diff --name-only --diff-filter=ACM | grep -e '\.py$')
 
 if [ -n "$FILES" ]; then
-    flake8 -r $FILES
+    echo "======================="
+    echo "======= CHECKING FILES: $FILES"
+    echo "======================="
 fi
