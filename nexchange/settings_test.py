201f5469f96e9477947805e378c265e2865f48b8
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-06-12 21:18:26 -0300

    Comment about using sqlite3.

--- nexchange/settings_test.py
+++ nexchange/settings_test.py
@@ -1,5 +1,6 @@
 from .settings import *
 
+# When testing, use sqlite3 so the satabase is loaded in memory for better performance
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
