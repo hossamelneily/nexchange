2d1445cd7dcc4d61cac5dec719bb9f99c17c3bac
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-06-12 21:02:43 -0300

    Check for non cached files.

--- static-validation.sh
+++ static-validation.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# Auto-check for pep8 so I don't check in bad code
 
 FILES_CACHED=$(git diff --cached --name-only --diff-filter=ACM | grep -e '\.py$')
+
+# If we only check for '--cached', when one does 'git commit -a' static validation will be skiped...
 FILES=$(git diff --name-only --diff-filter=ACM | grep -e '\.py$')
 
-
-OPTIONS="--ignore=F403"
+OPTIONS="--ignore=F403" 
 
 
 if [ -n "$FILES" ] || [ -n "$FILES_CACHED" ] ; then
