88a563b786d6eb4a918a1e3a6dd64d23d40bb2bb
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-07-12 21:58:18 -0300

    precommit: executing inside running container.

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -2,15 +2,14 @@
 
 RUNNING_CONTAINER=$(docker ps -q --filter "ancestor=pitervergara/geodjango:nexchange" --filter name="wercker-pipeline-" --filter status=running)
 
-FILES_CACHED=$(git diff --cached --name-only --diff-filter=ACM | grep -e '\.py$')
-FILES=$(git diff --name-only --diff-filter=ACM | grep -e '\.py$')
-
-
 function use_running_container {
-    
-    docker exec -t ${RUNNING_CONTAINER} bash -c "cd /pipeline/source && ./static-validation.sh" &&
-        docker exec -t ${RUNNING_CONTAINER} bash -c "cd /pipeline/source && DJANGO_SETTINGS_MODULE=nexchange.settings_test python manage.py test -v 3" &&
-            docker exec -t ${RUNNING_CONTAINER} bash -c "cd /pipeline/source && PHANTOMJS_BIN=node_modules/.bin/phantomjs  npm run-script test"
+    static_validation_cmd="cd /pipeline/source && ./static-validation.sh"
+    backend_tests="cd /pipeline/source && DJANGO_SETTINGS_MODULE=nexchange.settings_test python manage.py test -v 3"
+    frontend_tests="cd /pipeline/source && PHANTOMJS_BIN=node_modules/.bin/phantomjs  npm run-script test"
+
+    docker exec -t ${RUNNING_CONTAINER} bash -c "${static_validation_cmd}" &&
+        docker exec -t ${RUNNING_CONTAINER} bash -c "${backend_tests}" &&
+            docker exec -t ${RUNNING_CONTAINER} bash -c "${frontend_tests}"
 }
 
 function use_wercker {
@@ -20,19 +19,9 @@ function use_wercker {
 
 
 if [ -z "${RUNNING_CONTAINER}" ]; then
+    echo -e "\e[33mDid not found a running container for the nexchange project. Starting one to execute the pre-commit hook.\e[39m"
     use_wercker
 else
+    echo -e "\e[32mRunning pre-commit hook inside the container with id ${RUNNING_CONTAINER}, which is believed to be nexchange dev.\e[39m"
     use_running_container
 fi
-
-#if [ -n "$FILES" ] || [ -n "$FILES_CACHED" ] ; then
-#    wercker build --direct-mount --pipeline static-validation &&
-#        wercker build --direct-mount --pipeline tests
-#else
-#    echo "======================="
-#    echo "======= No .py FILES TO COMMIT. SKIPING STATIC VALIDATION. "
-#    echo "======================="
-#    wercker build --direct-mount --pipeline tests
-#fi
-
-
