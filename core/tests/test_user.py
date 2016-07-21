51cfbbc495402651673162ed907fd10ef903bcf5
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-07-21 00:52:51 -0300

    tests: Fix and add test to profile custom manager.

--- core/tests/test_user.py
+++ core/tests/test_user.py
@@ -102,6 +102,20 @@ class ShortRegistrationTestCase(TestCase):
         pass
 
 
+class ProfileFindTestCase(UserBaseTestCase):
+
+    def setUp(self):
+        super(ProfileFindTestCase, self).setUp()
+
+        self.profile = Profile()
+        self.profile.user = self.user
+        self.profile.save()
+
+    def test_finds_profile_by_natural_key(self):
+        profile = Profile.objects.get_by_natural_key(self.username)
+        self.assertEqual(profile, self.profile)
+
+
 class LoginTestCase(UserBaseTestCase):
 
     def test_login_should_display_correctly(self):
