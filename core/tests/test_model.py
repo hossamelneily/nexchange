1902abf01105e51f9787c0331d5aba2ac54e07b4
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-07-22 01:22:31 -0300

    tests: add test for TimezoneMiddleware and move DataProviderDecorator tests.

--- core/tests/test_model.py
+++ core/tests/test_model.py
@@ -8,11 +8,6 @@ from datetime import timedelta
 import time
 from django.conf import settings
 from .utils import UserBaseTestCase, OrderBaseTestCase, data_provider
-from unittest import mock
-
-from ticker.models import Price
-from datetime import datetime
-from django.utils.dateformat import format
 
 
 class OrderValidatePaymentTestCase(UserBaseTestCase, OrderBaseTestCase):
@@ -281,70 +276,3 @@ class PaymentMethodTestCase(UserBaseTestCase, OrderBaseTestCase):
         pref.save()
 
         self.assertEqual(self.payment_method, pref.payment_method)
-
-
-class DataProviderDecoratorTestCase(TestCase):
-
-    def test_calls_fn_data_provider(self):
-        func_that_provides_data = mock.MagicMock(return_value='x')
-        func_that_is_decorated = mock.MagicMock()
-
-        decorator = data_provider(func_that_provides_data)
-        decorated = decorator(func_that_is_decorated)
-        decorated(None)
-
-        self.assertTrue(func_that_provides_data.called)
-
-    def test_calls_decorated_func_with_data_provided(self):
-        param = '123'
-        expected_calls = [((None, '1'),), ((None, '2'),), ((None, '3'),)]
-
-        func_that_provides_data = mock.MagicMock(return_value=param)
-        func_that_is_decorated = mock.MagicMock()
-
-        decorator = data_provider(func_that_provides_data)
-        decorated = decorator(func_that_is_decorated)
-
-        decorated(None)
-
-        self.assertEqual(func_that_is_decorated.call_count, 3)
-        self.assertEqual(func_that_is_decorated.call_args_list, expected_calls)
-
-    def test_catches_assertionError_on_decorated(self):
-
-        func_that_provides_data = mock.MagicMock(return_value='1')
-        func_that_is_decorated = mock.MagicMock(
-            side_effect=AssertionError('Boom!'))
-
-        decorator = data_provider(func_that_provides_data)
-        decorated = decorator(func_that_is_decorated)
-
-        with self.assertRaises(AssertionError):
-            decorated(None)
-
-
-class PriceTestCase(TestCase):
-
-    def setUp(self):
-
-        self.created_on = datetime.now()
-        self.data = {
-            'better_adds_count': 1,
-            'price_rub': 41758.2,
-            'price_usd': 650.88846,
-            'rate': 64.15569266660528,
-            'type': 'B',
-            'created_on': self.created_on
-        }
-
-        self.price = Price(**self.data)
-        self.price.save()
-
-    def test_returns_unix_time(self):
-        self.assertEqual(self.price.unix_time, format(self.created_on, 'U'))
-
-    def test_returns_price_in_usd(self):
-        self.assertEqual(self.price.price_usd_formatted, 650.89)
-
-    def test_returns_price_in_rubd(self):
-        self.assertEqual(self.price.price_rub_formatted, 41758.2)
