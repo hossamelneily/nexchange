1eba71ccf00c752809b4ce85c2894b3bd3d325ad
Piter Oliveira Vergara <pitervergara@code4fun.com.br>
2016-07-27 00:18:55 -0300

    more_tests: Add some more tests.

--- ticker/tests/test_model.py
+++ ticker/tests/test_model.py
@@ -1,6 +1,5 @@
 from django.utils.dateformat import format
 from django.test import TestCase
-from datetime import datetime
 
 from ticker.models import Price
 
@@ -9,21 +8,19 @@ class PriceTestCase(TestCase):
 
     def setUp(self):
 
-        self.created_on = datetime.now()
         self.data = {
             'better_adds_count': 1,
             'price_rub': 41758.2,
             'price_usd': 650.88846,
-            'rate': 64.15569266660528,
-            'type': 'B',
-            'created_on': self.created_on
+            'type': 'B'
         }
 
         self.price = Price(**self.data)
         self.price.save()
 
     def test_returns_unix_time(self):
-        self.assertEqual(self.price.unix_time, format(self.created_on, 'U'))
+        self.assertEqual(self.price.unix_time,
+                         format(self.price.created_on, 'U'))
 
     def test_returns_price_in_usd(self):
         self.assertEqual(self.price.price_usd_formatted, 650.89)
