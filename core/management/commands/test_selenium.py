dec1487638d9e0d04cee9056d0b6077b2d09778b
kydim <kyz1990>
2016-09-01 18:13:10 +0300

    test buy

--- core/management/commands/test_selenium.py
+++ core/management/commands/test_selenium.py
@@ -3,82 +3,111 @@ from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.desired_capabilities
-#  import DesiredCapabilities
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
+import sys
 from django.core.management.base import BaseCommand
-# from core.models import SmsToken
+from core.models import SmsToken
 
 
-class TestBuyOrder(BaseCommand):
+class Command(BaseCommand):
 
     def __init__(self, *args, **kwargs):
-        super(TestBuyOrder, self).__init__(*args, **kwargs)
+        super(Command, self).__init__(*args, **kwargs)
+        self.issavescreen = False
         self.screenpath = os.path.join(
             os.path.dirname(__file__), 'Screenshots')
         if not os.path.exists(self.screenpath):
             os.makedirs(self.screenpath)
-        # dcap = dict(DesiredCapabilities.PHANTOMJS)
-        # self.driver = webdriver.PhantomJS(
-        # service_args=['--ignore-ssl-errors=true',
-        #  '--ssl-protocol=any'],desired_capabilities=dcap)
-        self.driver = webdriver.Firefox()
-        self.driver.set_window_size(1400, 1000)
-        self.driver.set_page_load_timeout(10)
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64)' \
+                     ' AppleWebKit/537.36 (KHTML, like Gecko)' \
+                     ' Chrome/37.0.2062.120 Safari/537.36'
+        self.dcap = dict(DesiredCapabilities.PHANTOMJS)
+        self.dcap["phantomjs.page.settings.userAgent"] = user_agent
         self.timeout = 5
-        self.wait = WebDriverWait(self.driver, self.timeout)
 
     def handle(self, *args, **options):
-        self.driver.get('http://localhost:8000/')
-        self.driver.get_screenshot_as_file(
-            os.path.join(self.screenpath, 'main.png'))
-        btbuy = self.wait.until(
+        banks = ['alfa-bank', 'Sberbank', 'Qiwi wallet']
+        print('Test buy')
+        for b in banks:
+            try:
+                self.checkbuy(b)
+            except Exception as e:
+                print(b + " " + str(e))
+                sys.exit(1)
+
+    def checkbuy(self, paymethod):
+        print(paymethod)
+        driver = webdriver.PhantomJS(
+            service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'],
+            desired_capabilities=self.dcap)
+        driver.set_window_size(1400, 1000)
+        driver.set_page_load_timeout(10)
+
+        wait = WebDriverWait(driver, self.timeout)
+
+        driver.get('http://localhost:8000/')
+        self.doscreenshot(driver, 'main_')
+        btbuy = wait.until(
             EC.element_to_be_clickable((By.CLASS_NAME, 'trigger-buy')))
         btbuy.click()
-        self.driver.get_screenshot_as_file(
-            os.path.join(self.screenpath, 'after buy click.png'))
-        payments = self.driver.\
+        sleep(1)
+        self.doscreenshot(driver, 'after buy click')
+        payments = driver.\
             find_elements_by_class_name('payment-method-icon')
         paybank = None
         for p in payments:
             try:
-                alt = p.get_attribute('alt')[:4].lower()
+                alt = p.get_attribute('alt').lower()
             except:
                 continue
-            if alt == 'alfa':
+            if alt == paymethod.lower() and p.is_displayed():
                 paybank = p
                 break
-        if paybank is None:
-            return False
-        print(paybank.get_attribute('alt'))
-        self.driver.get_screenshot_as_file(
-            os.path.join(self.screenpath, 'after bank click.png'))
+
+        sleep(2)
         paybank.click()
-        self.waitJS()
-        # wait = WebDriverWait(self.driver, 10)
-        # self.wait.until(
-        # EC.invisibility_of_element_located((By.NAME, 'phone')))
-        sleep(3)
-        phone = self.driver.find_elements_by_class_name('phone')
-        print(phone[1].get_attribute('class'))
-        phone[1].send_keys('1111111111')
-        # phone.click()
-        self.driver.get_screenshot_as_file(
-            os.path.join(self.screenpath, 'after input number.png'))
-        btnes = self.driver.find_elements_by_class_name('create-acc')
+        sleep(2)
+        self.doscreenshot(driver, 'after bank click')
+        phone = driver.find_elements_by_class_name('phone')
+
+        test_phone = '1111'
+        phone[1].send_keys(test_phone)
+
+        self.doscreenshot(driver, 'after input phone number')
+        btnes = driver.find_elements_by_class_name('create-acc')
+        # enter send sms
         for btsendsms in btnes:
             if btsendsms.get_attribute('class')\
                     .find('btn-primary') > -1:
                 btsendsms.click()
                 break
+        sleep(2)
         # input sms code
+        last_sms = SmsToken.objects.filter().latest('id').sms_token
+        ver_code = wait.until(EC.element_to_be_clickable(
+            (By.ID, 'verification_code')))
 
-    def waitJS(self):
-        for i in range(0, 10):
-            done = \
-                self.driver.execute_script(
-                    "return document.readyState") == "complete"
-            if done:
+        ver_code.send_keys(last_sms)
+
+        self.doscreenshot(driver, 'after input code number')
+
+        driver.execute_script('window.submit_phone()')
+        sleep(1)
+        self.doscreenshot(driver, 'after verifycate phone')
+        # press buy
+        bt_buys = driver.find_elements_by_class_name('buy-go')
+        for b in bt_buys:
+            if b.get_attribute('class') \
+                    .find('place-order') > -1:
+                b.click()
                 break
-            else:
-                sleep(1)
+        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'unique_ref')))
+        self.doscreenshot(driver, 'End')
+        print('Ok')
+        driver.close()
+
+    def doscreenshot(self, driver, filename):
+        if self.issavescreen:
+            driver.get_screenshot_as_file(
+                os.path.join(self.screenpath, filename + '.png'))
