eb05031484f728cd0722966065ac75de6221963c
Oleg Belousov <Hellgy@gmail.com>
2016-10-19 22:33:14 +0200

    Update models.py

--- core/common/models.py
+++ core/common/models.py
@@ -57,7 +57,7 @@ class Currency(TimeStampedModel, SoftDeletableModel):
     objects = CurrencyManager()
     code = models.CharField(max_length=3)
     name = models.CharField(max_length=10)
-    min_confirmations = models.IntegerField(default=0)
+    min_confirmations = models.IntegerField(default=0, blank=True, null=True)
 
     def natural_key(self):
         return self.code
