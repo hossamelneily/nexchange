d4362b8cc8026729b669e754ed013c23236377a6
Oleg Belousov <oleg@onit.ws>
2016-12-17 01:01:22 +0100

    fix graph initially not rendering, fix CMS page rendering

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from articles import views
 
-article_patterns = [
+article_urls = [
     url(r'^(?P<page_name>.+)/$', views.cms_page,
         name='articles.cms_page'),
 ]
--- nexchange/urls.py
+++ nexchange/urls.py
@@ -23,6 +23,7 @@ from payments.urls import payment_urls
 from referrals.urls import referrals_api_patterns, referral_urls
 from orders.urls import order_urls
 from accounts.urls import account_urls
+from articles.urls import article_urls
 from django.conf import settings
 from django.conf.urls.static import static
 import os
@@ -46,6 +47,7 @@ urlpatterns = i18n_patterns(
     url(r'^accounts/', include(account_urls)),
     url(r'^payments/', include(payment_urls)),
     url(r'^referrals/', include(referral_urls)),
+    url(r'^articles/', include(article_urls)),
 
     url(r'^api/v1/', include(api_patterns)),
     url(r'^api/v1/menu', core.views.ajax_menu, name='core.menu'),
