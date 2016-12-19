cf25f632a7c7971eef2ba9fb6d2b1d418324d052
Oleg Belousov <oleg@onit.ws>
2016-12-19 20:40:04 +0200

    Backend and frontend implementation of the OpenGraph component for the CMS

--- articles/views.py
+++ articles/views.py
@@ -7,8 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 
 def cms_page(request, page_name):
     template = get_template('articles/cms_default.html')
-    page = head = body = written_by = None
-
+    page = None
     try:
         page = CmsPage.objects.get(
             name=page_name,
@@ -17,11 +16,6 @@ def cms_page(request, page_name):
     except CmsPage.DoesNotExist:
         if not settings.DEBUG:
             raise Http404(_("Page not found"))
-
-    if page:
-        head = page.head
-        body = page.body
-        written_by = page.written_by
 
     cms_menu = []
     all_cms = [sf for sf in settings.CMSPAGES.values()]
@@ -33,9 +27,8 @@ def cms_page(request, page_name):
             break
 
     context = {
+        'page': page,
         'cmsmenu': cms_menu,
-        'head': head,
-        'body': body,
-        'written_by': written_by, }
+    }
 
     return HttpResponse(template.render(context, request))
