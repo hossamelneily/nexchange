ec389f056bd0fee8d1d7f62399a3aff1850dca15
Ihar <westdvina@gmail.com>
2016-12-24 08:42:00 +0300

    support changes

--- support/admin.py
+++ support/admin.py
@@ -3,7 +3,9 @@ from .models import Support
 
 
 class SupportAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created', 'is_resolved']
+    list_display = ['name', 'user', 'created', 'is_resolved']
+    list_filter = ('is_resolved',)
+    search_fields = ['name', ]
 
 
 admin.site.register(Support, SupportAdmin)
--- support/models.py
+++ support/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from orders.models import Order
+from django.utils.translation import ugettext_lazy as _
 
 
 class Support(models.Model):
     user = models.ForeignKey(User, verbose_name='user', null=True, blank=True)
     order = models.OneToOneField(Order, verbose_name='order',
                                  null=True, blank=True)
-    name = models.CharField('Name', max_length=50)
-    email = models.EmailField('Email')
-    telephone = models.CharField('Telephone', max_length=50,
+    name = models.CharField(_('Name*'), max_length=50)
+    email = models.EmailField(_('Email*'))
+    telephone = models.CharField(_('Telephone'), max_length=50,
                                  null=True, blank=True)
-    subject = models.CharField('Subject', max_length=50, null=True, blank=True)
-    message = models.TextField('Message')
+    subject = models.CharField(_('Subject'), max_length=50,
+                               null=True, blank=True)
+    message = models.TextField(_('Message*'))
     is_resolved = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
