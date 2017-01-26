02821e86bfa74584b72ad75e755c31d557c45cc9
Oleg Belousov <oleg@onit.ws>
2017-01-26 20:44:27 +0200

    send celery task logs to /dev/stdout, fix mapping error in nginx

--- nexchange/tasks/base.py
+++ nexchange/tasks/base.py
@@ -1,12 +1,21 @@
 from __future__ import absolute_import
 from celery import Task
 import logging
+import sys
 
 
 class BaseTask(Task):
 
     def __init__(self, *args, **kwargs):
+        # Logging
         self.logger = logging.getLogger(
             self.__class__.__name__
         )
+
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s'
+                                      ' - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
         super(BaseTask, self).__init__(*args, **kwargs)
