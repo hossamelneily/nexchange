272001ecb091840472792359264d1015c0bbfaba
Oleg Belousov <oleg@onit.ws>
2017-01-27 16:01:02 +0200

    return status code form tests sh script

--- run-tests.sh
+++ run-tests.sh
@@ -2,3 +2,16 @@ coverage erase
 coverage run --source="." --omit="src/**" manage.py test -v=3 --pattern="**/tests/**py" --settings=nexchange.settings_test
 coverage report
 coverage html -d cover
+
+#!/bin/bash
+
+touch /root/test 2> /dev/null
+
+if [ $? -eq 0 ]
+then
+   eho "tests passed"
+   exit 0
+else
+  echo "Tests failed"
+  exit 1
+fi
