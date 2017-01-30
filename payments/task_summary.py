983aa941f7440ef6f3c83444f3c28ed8ae80ab21
Oleg Belousov <oleg@onit.ws>
2017-01-30 13:26:53 +0200

    hack celery to work

--- payments/task_summary.py
+++ payments/task_summary.py
@@ -1,1 +1,15 @@
-from .tasks.import_payments import run_okpay, run_payer # noqa
+from payments.tasks.generic.ok_pay import OkPayPaymentChecker
+from payments.tasks.generic.payeer import PayeerPaymentChecker
+from celery import shared_task
+
+
+@shared_task()
+def run_payeer():
+    instance = PayeerPaymentChecker()
+    return instance.run()
+
+
+@shared_task()
+def run_okpay():
+    instance = OkPayPaymentChecker()
+    return instance.run()
