58b5810dd5dea26132c809a6532db44617b8f8d1
Oleg Belousov <oleg@onit.ws>
2017-02-23 17:16:12 +0200

    expose Flag model in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from core.models import Currency, Transaction, Address, Pair
+from core.common.models import Flag
 
 
 class TaskMetaAdmin(admin.ModelAdmin):
@@ -10,4 +11,5 @@ admin.site.register(Currency)
 admin.site.register(Transaction)
 admin.site.register(Address)
 admin.site.register(Pair)
+admin.site.register(Flag)
 admin.autodiscover()
