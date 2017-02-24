b2b708e9ecacebb787458507ebf8038e5612967d
Oleg Belousov <oleg@onit.ws>
2017-02-24 11:03:29 +0200

    remove un-used TaskMeta admin

--- core/admin.py
+++ core/admin.py
@@ -3,10 +3,6 @@ from core.models import Currency, Transaction, Address, Pair
 from core.common.models import Flag
 
 
-class TaskMetaAdmin(admin.ModelAdmin):
-    readonly_fields = ('result',)
-
-
 admin.site.register(Currency)
 admin.site.register(Transaction)
 admin.site.register(Address)
