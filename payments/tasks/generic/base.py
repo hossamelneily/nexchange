a83a64d66ae67b64f4ba303b7243b60dbbc4fd29
Oleg Belousov <oleg@onit.ws>
2017-02-26 17:33:41 +0200

    fix tests

--- payments/tasks/generic/base.py
+++ payments/tasks/generic/base.py
@@ -182,7 +182,7 @@ class BasePaymentChecker(BaseTask):
     def validate_transaction(self):
         success = self.validate_success()
         valid_beneficiary = self.validate_beneficiary()
-        if not int(self.data['amount']):
+        if not int(self.data['amount_cash']):
             self.logger.info('Payment {} is debit'.format(self.data))
         if not success:
             self.logger.error('Payment {} is not success'.format(self.data))
