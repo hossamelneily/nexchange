fe7ef858e9bece546084147d88f62b0d91219eb4
Oleg Belousov <oleg@onit.ws>
2017-03-16 09:48:47 +0200

    Order admin

--- orders/admin.py
+++ orders/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from orders.models import Order
 
-admin.site.register(Order)
+
+class OrderAdmin(admin.ModelAdmin):
+    raw_id_fields = ("price",)
+
+
+admin.site.register(Order, OrderAdmin)
