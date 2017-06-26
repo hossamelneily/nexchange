9b8202d146e8d0d9c7d04425b4a997cb9217b28a
Šarūnas Ažna <sarunas.azna@gmail.com>
2017-06-26 22:35:13 +0200

    verification ImageFields --> FileFields

--- verification/models.py
+++ verification/models.py
@@ -23,10 +23,10 @@ class Verification(TimeStampedModel, SoftDeletableModel):
         return self._get_file_name(filename, root)
 
     user = models.ForeignKey(User)
-    identity_document = models.ImageField(
+    identity_document = models.FileField(
         upload_to=identity_file_name
     )
-    utility_document = models.ImageField(
+    utility_document = models.FileField(
         upload_to=_utility_file_name
     )
     id_status = models.CharField(choices=TYPES, max_length=10, null=True,
