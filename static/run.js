ab3d2400ae6c2b313cb93be49954790c8bab7a8e
Karolis Ramanauskas <karolisr@live.com>
2017-07-14 16:41:07 +0300

    Fix orders calendar styling.

--- static/run.js
+++ static/run.js
@@ -8,6 +8,8 @@ import '../node_modules/bootstrap-toggle/css/bootstrap-toggle.min.css';
 import '../node_modules/toastr/build/toastr.css';
 import '../node_modules/bootstrap-social/bootstrap-social.css';
 import '../node_modules/cryptocoins-icons/cryptocoins.css';
+import '../node_modules/jquery-ui/themes/base/theme.css';
+import '../node_modules/jquery-ui/themes/base/datepicker.css';
 
 window.countryCode = document.getElementsByTagName("html")[0].getAttribute("lang");
 
