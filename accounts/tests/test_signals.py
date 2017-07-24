5e3a190bf89bff3458763528ae565a3b0eff3abf
Šarūnas Ažna <sarunas.azna@gmail.com>
2017-07-24 12:00:31 +0300

    2 users at the same time test

--- accounts/tests/test_signals.py
+++ accounts/tests/test_signals.py
@@ -6,6 +6,8 @@ from core.models import AddressReserve as Cards
 from core.models import Currency
 from accounts.models import Profile
 from django.conf import settings
+from unittest.mock import patch
+from accounts.task_summary import check_cards_uphold_invoke
 
 
 class RenewReserveTestCase(OrderBaseTestCase):
@@ -41,3 +43,33 @@ class RenewReserveTestCase(OrderBaseTestCase):
             self.len_crypto_curencies * settings.EMERGENCY_CARDS_RESERVE_COUNT
         self.assertEqual(len_user_cards, len_expected)
         self.assertEqual(len_reserve_cards, 0)
+
+    @requests_mock.mock()
+    def test_2_users_1_card(self, mock):
+        with patch(
+                'core.signals.allocate_wallets.'
+                'AddressReserve.objects.filter') as filter:
+            with patch(
+                    'core.signals.allocate_wallets.'
+                    'renew_cards_reserve') as allocate_reserver:
+                filter.return_value = []
+                allocate_reserver.return_value = True
+                user1 = User(username='Parallel1')
+                user1.save()
+                profile1 = Profile(user=user1)
+                profile1.save()
+        # user1 created, but has no cards
+        self.assertEqual(len(user1.addressreserve_set.all()), 0)
+        self._mock_cards_reserve(mock)
+        # user2 steals user1 cards
+        renew_cards_reserve(expected_reserve=1)
+        user2 = User(username='Parallel2')
+        user2.save()
+        profile2 = Profile(user=user2)
+        profile2.save()
+        self.assertEqual(len(user2.addressreserve_set.all()),
+                         self.len_crypto_curencies)
+        self._mock_cards_reserve(mock)
+        check_cards_uphold_invoke.apply()
+        self.assertEqual(len(user1.addressreserve_set.all()),
+                         self.len_crypto_curencies)
