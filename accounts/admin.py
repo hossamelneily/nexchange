b2297158cc35c80b042a0e6acc37c9c669ca2003
SarunasAzna <SarunasAzna@users.noreply.github.com>
2017-09-26 17:53:51 +0200

    Swagger api documentation / Superuser field block on Admin Panel (#527)
    * Swagger format API documentation
    
    * Fix apiary and swagger files
    
    * Remove is_superuser from non superusers admin view

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from accounts.forms import BaseProfileForm
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
 from accounts.models import Profile, SmsToken
 
@@ -10,6 +13,30 @@ class ProfileAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'time_zone')
 
 
+class NexchangeUserAdmin(UserAdmin):
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+
+        if request.user.is_superuser:
+            perm_fields = ('is_active', 'is_staff', 'is_superuser',
+                           'groups', 'user_permissions')
+        else:
+            # modify these to suit the fields you want your
+            # staff user to be able to edit
+            perm_fields = ('is_active', 'is_staff', 'groups',
+                           'user_permissions')
+
+        return [(None, {'fields': ('username', 'password')}),
+                (_('Personal info'),
+                 {'fields': ('first_name', 'last_name', 'email')}),
+                (_('Permissions'), {'fields': perm_fields}),
+                (_('Important dates'),
+                 {'fields': ('last_login', 'date_joined')})]
+
+
 # Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, NexchangeUserAdmin)
 admin.site.register(SmsToken)
 admin.site.register(Profile, ProfileAdmin)
