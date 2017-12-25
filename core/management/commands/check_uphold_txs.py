307bdc75eb74b8768e8a71abc32f887eb6b774c7
SarunasAzna <SarunasAzna@users.noreply.github.com>
2017-12-25 13:59:05 +0100

    Uphold audit (#559)
    * WIP: audit untracked uphold transactions
    
    * Address to printout of untracked transactions

--- core/management/commands/check_uphold_txs.py
+++ core/management/commands/check_uphold_txs.py
@@ -16,7 +16,14 @@ class Command(BaseCommand):
                uphold_deposit_txs if not tx.tx_id_api]
         return res
 
-    def get_last_txs(self, count=1400):
+    def get_uphold_transactions_ids_lists(self):
+        uphold_txs = Transaction.objects.filter(
+            currency__wallet='api1')
+        tx_ids = [tx.tx_id for tx in uphold_txs]
+        tx_api_ids = [tx.tx_id_api for tx in uphold_txs]
+        return tx_ids, tx_api_ids
+
+    def get_last_txs(self, count=2500):
         last_txs = []
         for i in range(0, count, 50):
             items = 'items={}-{}'.format(i, i + 49)
@@ -27,15 +34,40 @@ class Command(BaseCommand):
 
     def get_last_txs_ids(self):
         last_txs = self.get_last_txs()
-        return [
+        last_ids = [
             tx.get('params', {}).get('txid')
             for tx in last_txs if 'txid' in tx.get('params', {})
         ]
+        last_uphold_ids = [
+            {
+                'tx_id_api': tx.get('id', 'None'),
+                'tx_id': tx.get('params', {}).get('txid', 'None'),
+                'type': tx.get('params', {}).get('type', 'None'),
+                'currency': tx.get('params', {}).get('currency', 'None'),
+                'amount': tx.get('destination', {}).get('amount', 'None'),
+                'address': tx.get('destination', {}).get('address', 'None'),
+                'created_at': tx.get('createdAt'),
+            } for tx in last_txs
+        ]
+        return last_ids, last_uphold_ids
 
     # A command must define handle()
     def handle(self, *args, **options):
-        txs = self.get_uphold_deposit_transactions()
-        last_ids = self.get_last_txs_ids()
-        suspicious_txs = [tx for tx in txs if tx.get('tx_id') not in last_ids]
+        deposit_txs = self.get_uphold_deposit_transactions()
+        last_ids, last_uphold_ids = self.get_last_txs_ids()
+        suspicious_txs = [
+            tx for tx in deposit_txs if tx.get('tx_id') not in last_ids]
         print('Suspicious txs:')
         print(suspicious_txs)
+        tx_ids, tx_api_ids = self.get_uphold_transactions_ids_lists()
+        not_tracked_txs = [
+            tx for tx in last_uphold_ids if all([
+                tx.get('tx_id') not in tx_ids,
+                tx.get('tx_id_api') not in tx_api_ids,
+                tx.get('type') != 'transfer'
+            ])
+        ]
+        print('Not tracked txs:')
+        for tx in not_tracked_txs[::-1]:
+            print(tx['created_at'], tx['currency'], tx['amount'],
+                  tx['address'], tx['tx_id'], tx['tx_id_api'], tx['type'])
